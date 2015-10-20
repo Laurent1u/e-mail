f8fda980db54cfa99e7f8ec4a2b810e537d10dcc
Laurentiu <pascu.laurentiu17@gmail.com>
2015-10-20 22:15:26 +0300

    modificat pagina login + putina formatare

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php 
+// verific daca s-a trimis formularul
+if(isset($_POST['trimis'])){
+	print_r($_POST);
+	
+	require_once ('connect.php');
+	// variabile login
+	$user = mysqli_real_escape_string($_POST['user']);
+	$password = $_POST['password'];
+	
+	// verific username si parola
+	if(empty($user)){
+		$errors[] = "Completati numele!";
+	}else{
+		// verific daca exista in DB
+		$query = "SELECT * FROM admin WHERE nume = '$user'";
+		$result = mysqli_query($link, $query);
+	}
+	
+	// verific parola
+	if(empty($password)){
+		$errors[] = "Completati parola!";
+	}
+}
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
@@ -11,6 +36,14 @@
  				<tr>
  					<td>
  					<p>Conectare</p>
+ 					<?php 
+ 					// afisez erorile daca exista
+ 					if (isset($errors)){
+ 							echo "<p>$errors</p>";
+ 					}
+ 					
+ 					?>
+ 					<form action="index.php" method="POST">
  					<label for="user">Nume: </label>
  					<input type="text" name="user" id="user" />
  					<br />
@@ -18,6 +51,8 @@
  					<input type="password" name="password" id="password" />
  					<br />
  					<input type="submit" value="Intra" />
+ 					<input type="hidden" name="trimis" value="true" />
+ 					</form>
  					</td>
  				</tr>
  			</table>
