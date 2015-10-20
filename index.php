bc82f000142edfb5141fbee21dead31922f8ae06
Laurentiu <pascu.laurentiu17@gmail.com>
2015-10-20 20:06:44 +0300

    verificare initiala

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<link href="style.css" rel="stylesheet" type="text/css" />
+<title>Trimite E-mail</title>
+</head>
+    <body>
+ 		<div class="container">
+ 			<table align="center">
+ 				<tr>
+ 					<td>
+ 					<p>Conectare</p>
+ 					<label for="user">Nume: </label>
+ 					<input type="text" name="user" id="user" />
+ 					<br />
+ 					<label for="password">Parola: </label>
+ 					<input type="password" name="password" id="password" />
+ 					<br />
+ 					<input type="submit" value="Intra" />
+ 					</td>
+ 				</tr>
+ 			</table>
+ 		</div>
+    </body>
+</html>
