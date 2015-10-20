27139e20a658a58021f9a2c9c5e7df0001959f9d
Laurentiu <pascu.laurentiu17@gmail.com>
2015-10-20 22:14:49 +0300

    conectare db

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = 'localhost:8080';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'email';
+
+$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+// verific daca s-a realizat conexiunea
+if(mysqli_connect_error($link)){
+	die("Conexiune esuata");
+}
+// var_dump($link);
+
+?>
