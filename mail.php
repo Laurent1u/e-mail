f472bf2f7a65f05607fe58472e0158fca4d11c27
Laurentiu <pascu.laurentiu17@gmail.com>
2015-10-21 21:28:19 +0300

    erori variabile + se ruleaza codul automat fara sa apas tasta trimite

--- mail.php
+++ mail.php
@@ -1,3 +1,41 @@
+<?php 
+	// from email
+	$from = "test@gmail.com";
+	
+	// variabilele din formular
+	require_once 'connect.php';
+	
+	$subiect = $_POST['subiect'];
+	$mesaj = $_POST['mesaj'];
+	
+	// afisez from + subiect
+	
+	echo "From: " . $from . "<br />";
+	echo "Subiect: " . $subiect . "<hr />";
+	
+	//conectare la DB + selectare email-uri
+	$query = "SELECT email FROM lista_email";
+	$result = mysqli_query($link, $query) or die(mysqli_error($link));
+	
+	while ($rand = mysqli_fetch_array($result)){
+		$to = $rand['email'];
+		
+		mail($to, $subiect, $mesaj, 'From: ' . $from);
+		
+		// Afisam cui am trimis mail
+		
+		echo "Mail trimis catre: " . $to . "<hr />";
+	}
+	
+	// Afisam succes sau erori
+	
+	if(count($result)>0){
+		echo "<br />Mail-uri trimise!";
+	}else{
+		echo "<br />Nu s-au trimis mail-uri!";
+	}
+mysqli_close($link);
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
@@ -8,10 +46,10 @@
     <body>
     <h2>Compune e-mail</h2>
 <p>Scrie e-mailul pe care vrei sa il trimiti:</p>
-	<form action="mail.php" method="post">
+	<form action="mail.php" method="POST">
 	<div>
-		<label for="titlu">Titlu:</label>
-		<input type="text" name="titlu" id="titlu" />
+		<label for="titlu">Subiect:</label>
+		<input type="text" name="subiect" id="subiect" />
 		</div>
 		<label for="mesaj">Mesaj:</label><br />
 		<textarea name="mesaj" id="mesaj" rows="25" cols="60"></textarea>	
